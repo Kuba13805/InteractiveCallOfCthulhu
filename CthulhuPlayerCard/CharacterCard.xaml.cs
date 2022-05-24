ea4d780a4b5d83d9648434fb1caeb0c3527e73b4
Kuba13805 <79981760+Kuba13805@users.noreply.github.com>
2022-05-24 19:53:36 +0200

    Obsługa i łączenie do bazy danych
    Dodanie skryptu który pobiera id ostatniego rzędu w tabeli po przez łączenie z bazą danych z użyciem metody SqlConnection, SqlCommand, ExecuteScalar

---
+++ CthulhuPlayerCard/CharacterCard.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CthulhuPlayerCard
+{
+    /// <summary>
+    /// Interaction logic for CharacterCard.xaml
+    /// </summary>
+    public partial class CharacterCard : Window
+    {
+        string ChoosenID;
+        public CharacterCard()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ChoosenID = LoadCharacterWindow._LoadedCharacterID.ToString();
+            CharacterIdDisplay.Text = "Character ID: " + ChoosenID;
+        }
+    }
+}
--- CthulhuPlayerCard/LoadCharacterWindow.xaml.cs
+++ CthulhuPlayerCard/LoadCharacterWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
 
 namespace CthulhuPlayerCard
 {
@@ -19,7 +20,12 @@ namespace CthulhuPlayerCard
     /// </summary>
     public partial class LoadCharacterWindow : Window
     {
-        int LoadedCharacterID;
+        public static int _LoadedCharacterID;
+        private int LoadedCharacterID
+        {
+            get => _LoadedCharacterID;
+        }
+        bool success;
         public LoadCharacterWindow()
         {
             InitializeComponent();
@@ -40,9 +46,20 @@ namespace CthulhuPlayerCard
         {
 
         }
+        private int LastRowID()
+        {
+            int rowCount;
+            SqlConnection sqlConnection = new SqlConnection(@"Server=(LocalDb)\MSSQLLocalDB;Database=Projekt_Cthulhu;Trusted_Connection=Yes;");
+            sqlConnection.Open();
+            string sql = "SELECT COUNT(*) FROM ListaPostaci";
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandText = sql;
+            rowCount = Convert.ToInt32(sqlCommand.ExecuteScalar());
+            return rowCount;
+        }
         private void GettingCharacterId(string TextboxString)
         {
-            bool success = int.TryParse(TextboxString, out LoadedCharacterID);
+            success = int.TryParse(TextboxString, out _LoadedCharacterID);
             if (!success)
             {
                 string caption = "Wrong ID!";
@@ -50,16 +67,33 @@ namespace CthulhuPlayerCard
                 MessageBoxResult result = MessageBox.Show(message, caption);
                 CharacterIdInput.Clear();
             }
+            int rowCount = LastRowID();
+            if ((_LoadedCharacterID > rowCount) ^ (_LoadedCharacterID < 0))
+            {
+                MessageBox.Show("Character with this ID does not exist!", "Character not found!");
+                success = false;
+                CharacterIdInput.Clear();
+            }
         }
      
         private void LoadCharacterButton_Click(object sender, RoutedEventArgs e)
         {
             GettingCharacterId(CharacterIdInput.Text);
+            if (success)
+            {
+                CharacterCard LoadedCharacter = new CharacterCard();
+                LoadedCharacter.Show();
+                Close();
+            }
         }
 
         private void DeleteCharacterButton_Click(object sender, RoutedEventArgs e)
         {
             GettingCharacterId(CharacterIdInput.Text);
+            if (success)
+            {
+                DeleteCharacterData(LoadedCharacterID);
+            }
         }
 
         private void MainMenuButton_Click(object sender, RoutedEventArgs e)
@@ -67,6 +101,13 @@ namespace CthulhuPlayerCard
             MainWindow Window = new MainWindow();
             Window.Show();
             Close();
+        }
+        private void DeleteCharacterData(object sender)
+        {
+            if (MessageBox.Show("Are you sure you want to delete this character?", "Delete character", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                
+            }
         }
     }
 }
