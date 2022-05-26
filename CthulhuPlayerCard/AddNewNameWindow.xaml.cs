d4549485ebc7bb8916011486b278a866e4d104a1
Kuba13805 <79981760+Kuba13805@users.noreply.github.com>
2022-05-26 16:04:22 +0200

    Dodanie funkcji dodawania imienia, niedziałająca póki co

--- CthulhuPlayerCard/AddNewNameWindow.xaml.cs
+++ CthulhuPlayerCard/AddNewNameWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Data.SqlClient;
+using System.Text.RegularExpressions;
 
 namespace CthulhuPlayerCard
 {
@@ -37,34 +38,137 @@ namespace CthulhuPlayerCard
             imionaViewSource.View.MoveCurrentToFirst();
             
         }
-        private void CheckName(string givenName)
+        private bool CheckSpelling(string givenWord, string typeOfGivenWord)
         {
-            int SearchedId;
+            bool givenWordSpellingCheck = true;
+            if (givenWord == "")
+            {
+                MessageBox.Show('"' + typeOfGivenWord + '"' + " cannot be empty!", "Empty " + '"' + typeOfGivenWord + '"');
+            }
+            else
+            {
+                Regex rgx = new Regex("^[A-Za-z]*$");
+                bool cointainsSpecialCharacter = rgx.IsMatch(givenWord);
+                if (cointainsSpecialCharacter == false)
+                {
+                    MessageBox.Show("Word entered in " + '"' + typeOfGivenWord + '"' + " contains characters that are not allowed. Please, use only letters and avoid spaces.", "Wrong character!");
+                    givenWordSpellingCheck = false;
+                }
+            }
+            return givenWordSpellingCheck;
+        }
+        private string UpdateWord(string givenWord)
+        {
+            givenWord = givenWord.ToUpper();
+            char firstLetter;
+            string restOfNewGivenWord = "";
+            char[] arr;
+
+            arr = givenWord.ToArray();
+            for (int i = 1; i < arr.Length; i++)
+            {
+                restOfNewGivenWord = restOfNewGivenWord + arr[i];
+            }
+            firstLetter = arr[0];
+            string updatedWord = firstLetter + restOfNewGivenWord;
+            return updatedWord;
+        }
+        private bool CheckNameSex()
+        {
+            bool nameSexChecked = true;
+            EnterNameSex.Text.ToUpper();
+            if (EnterNameSex.Text == "M" || EnterNameSex.Text == "F")
+            {
+                nameSexChecked = true;
+            }
+            else
+            {
+                MessageBox.Show("Enter " + '"' + 'M' + '"' + "for male name and " + 'F' + '"' + "for female name.", "Wrong name sex");
+                nameSexChecked = false;
+            }
+            return nameSexChecked;
+        }
+        private int RandomId()
+        {
+            Random roll = new Random();
+            int rollResult = roll.Next(1, 9999);
+
             SqlConnection sqlConnection = new SqlConnection(@"Server=(LocalDb)\MSSQLLocalDB;Database=Projekt_Cthulhu;Trusted_Connection=Yes;");
             sqlConnection.Open();
-            string sql = "SELECT COUNT(*) FROM ListaPostaci where Id_postaci = " + LoadedCharacterID.ToString() + ";";
+            string sql = "Select Id_imienia FROM Imiona WHERE Id_imienia = " + rollResult + ";";
             SqlCommand sqlCommand = sqlConnection.CreateCommand();
             sqlCommand.CommandText = sql;
-            SearchedId = Convert.ToInt32(sqlCommand.ExecuteScalar());
+            int searchResult = Convert.ToInt32(sqlCommand.ExecuteScalar());
+            if (searchResult == 1)
+            {
+                RandomId();
+            }
+            return rollResult;
         }
         private void AddNameButton_Click(object sender, RoutedEventArgs e)
         {
+            bool checkNameSpelling = CheckSpelling(EnterNewName.Text, "Enter name");
+            bool checkOriginSpelling = CheckSpelling(EnterNameOrigin.Text, "Enter name origin");
+            bool checkMaleFemaleName = CheckNameSex();
+            if (checkNameSpelling == true && checkOriginSpelling == true && checkMaleFemaleName == true)
+            {
+                string nameToInput = UpdateWord(EnterNewName.Text);
+                string originToInput = UpdateWord(EnterNameOrigin.Text);
+                int idToInput = RandomId();
+                string nameSexToInput = EnterNameSex.Text;
+                InsertIntoNamesTable(nameToInput, originToInput, nameSexToInput, idToInput);
+            }
+
 
         }
-
+        private void InsertIntoNamesTable(string name, string origin, string nameSex, int id)
+        {
+            SqlConnection sqlConnection = new SqlConnection(@"Server=(LocalDb)\MSSQLLocalDB;Database=Projekt_Cthulhu;Trusted_Connection=Yes;");
+            sqlConnection.Open();
+            string sql = "insert into Imiona(Id_imienia, Imie, Pochodzenie_imienia, Plec_imienia) values(" + id + ',' + name + ',' + origin + ',' + nameSex + ");";
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandText = sql;
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            MessageBox.Show("Name " + '"' + name + '"' + " with id: " + '"' + id+ '"' + " has been added", "Name added");
+        }
         private void ClearTexboxButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if ((EnterID.Text == "") && (EnterNewName.Text == "") && ( EnterNameSex.Text == "") && (EnterNameOrigin.Text == ""))
+            {
+                MessageBox.Show("All spaces are clear", "All clear!");
+            }
+            else
+            {
+                EnterID.Clear();
+                EnterNewName.Clear();
+                EnterNameSex.Clear();
+                EnterNameOrigin.Clear();
+            }
         }
 
         private void DeleteNameButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if (MessageBox.Show("Are you sure you want to delete this name?", "Delete name?", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                int IdToDelete;
+                SqlConnection sqlConnection = new SqlConnection(@"Server=(LocalDb)\MSSQLLocalDB;Database=Projekt_Cthulhu;Trusted_Connection=Yes;");
+                sqlConnection.Open();
+                string sql = "DELETE FROM Imiona WHERE Id_imienia = " + EnterID.Text + ";";
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.CommandText = sql;
+                IdToDelete = Convert.ToInt32(sqlCommand.ExecuteScalar());
+                MessageBox.Show("Name with id: " + EnterID.Text + " has been removed.", "Character deleted!");
+                AddNewNameWindow Window = new AddNewNameWindow();
+                Window.Show();
+                Close();
+            }
         }
 
         private void BackButtonClick(object sender, RoutedEventArgs e)
         {
-
+            Options Window = new Options();
+            Window.Show();
+            Close();
         }
     }
 }
